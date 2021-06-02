e26def1cd231e74803aa40e04dd55527a3eae44d
Sofia <sofiathebass@berkeley.edu>
2021-06-02 15:25:22 +0200

    Finish Game

--- src/index.js
+++ src/index.js
@@ -25,8 +25,13 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        } 
         squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({squares: squares, xIsNext: !this.state.xIsNext,});
+        this.setState({
+            squares: squares, 
+            xIsNext: !this.state.xIsNext,});
     }
 
     renderSquare(i) {
